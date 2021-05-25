ffab1ac6a1c7354343b430c049b50b9dedcd5660
Ilham Nuruddin Al Huda <inurhuda00@gmail.com>
2021-05-25 12:09:03 +0700

    fix textSection

--- components/sections/TextSection.js
+++ components/sections/TextSection.js
@@ -1,13 +1,19 @@
 import { PortableText } from "../../utils/sanity";
 
 function TextSection(props) {
-  const { text } = props;
+  const { heading, label } = props;
   return (
     <div>
       <div className="container mx-auto px-6">
-        <div className="md:flex md:items-center">
-          {text && <PortableText blocks={text} />}
-        </div>
+        {heading && (
+          <div className="text-lg md:flex md:items-center font-medium">
+            heading
+          </div>
+        )}
+
+        {label && (
+          <div className="md:flex md:items-center font-thin">label</div>
+        )}
       </div>
     </div>
   );
